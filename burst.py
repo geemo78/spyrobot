73a0f9da2ed77c3b0d55fc40fef0f7ebca3c631a
Gio Andrei <gioandrei140@gmail.com>
2023-05-30 11:28:25 +0800

    Update burst.py

--- burst.py
+++ burst.py
@@ -3,7 +3,7 @@ import schedule
 import os
 
 cam = cv2.VideoCapture(1)
-cv2.namedWindow("Webcam")
+#cv2.namedWindow("Webcam")
 img_counter = 0
 save_dir = 'D:\yolov5\snaps'
 
@@ -12,13 +12,14 @@ def capture():
     img_name = "opencv_frame_{}.png".format(img_counter)
     #cv2.imwrite(img_name, frame)
     cv2.imwrite(os.path.join(save_dir,img_name), frame)
-    print("screenshot taken")
+    cv2.imshow("snap", frame)
+    print("screenshot taken and saved as", save_dir, "\\", img_name)
     img_counter += 1
 
 
 # Set up schedule before loop
 
-schedule.every(10).seconds.do(capture)
+schedule.every(5).seconds.do(capture)
 
 
 while True:
@@ -33,7 +34,7 @@ while True:
 
     k = cv2.waitKey(100)  # 1/10 sec delay; no need for separate sleep
 
-    if k % 256 == 27:
+    if k == 0:
         print("closing the app")
         break
 
