8fbdc3eff2ff5272eda7f22cea7f8b233d2878e5
Gio Andrei <gioandrei140@gmail.com>
2023-05-30 19:43:54 +0800

    Update spyrorecord.py

--- spyrorecord.py
+++ spyrorecord.py
@@ -1,18 +1,19 @@
 import cv2
 import msvcrt
+import time
 
-vid_capture = cv2.VideoCapture(0)
+vid_capture = cv2.VideoCapture(1)
 
 vid_cod = cv2.VideoWriter_fourcc(*'mp4v')
 output = cv2.VideoWriter(r"C:\Users\jerom\Dropbox\My PC (DESKTOP-VN5I5A5)\Desktop\python\clips\vid1.mp4", vid_cod, 20.0, (640,480))
 
-for i in range(1,1000):
-    # Capture each frame of webcam video
+# Capture each frame of webcam video
+
+for i in range(1,200):
+
     ret,frame = vid_capture.read()
     cv2.imshow("My cam video", frame)
     output.write(frame)
-    # Close and break the loop after pressing "x" key
-    #if cv2.waitKey(1) &0XFF == ord('x'):
-        #break
+        
 
-print("Video is being captured.")
+print("Video has been captured.")
